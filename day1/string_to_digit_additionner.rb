93db407f35db9b4563e64cc765a7558a2ecfe47f
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-12-01 13:55:23 +0100

    added a rough draft for ex 1

---
+++ day1/string_to_digit_additionner.rb
@@ -0,0 +1,31 @@
+class StringToDigitAdditionner
+  def initialize(raw_string)
+    @strings = split_strings(raw_string)
+  end
+
+  def sums
+    summed_up_numbers
+  end
+
+  def total
+    sums.sum
+  end
+
+  private
+
+  def summed_up_numbers
+    @strings.map { |str| sum_numbers_to_i(str.scan(/\d/)) }
+  end
+
+  def sum_numbers_to_i(numbers_to_sum)
+    (numbers_to_sum.first + numbers_to_sum.last).to_i
+  end
+
+  def find_digits(str)
+    str.scan(/\d/)
+  end
+
+  def split_strings(str)
+    str.split(/\n/)
+  end
+end
